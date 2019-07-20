7f5adaa23579d2a31bce19f1b44fd1c1c1d1b8d2
Lee Scholl <v1xiii@gmail.com>
2019-07-20 16:48:36 -0500

    Added mainscreencontroller to project

---
+++ src/view_controller/MainScreenController.java
@@ -0,0 +1,20 @@
+package view_controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainScreenController implements Initializable {
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
