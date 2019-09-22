a75e54660fad7c77407b5053def104cc57f0ea34
Lee Scholl <v1xiii@gmail.com>
2019-09-21 20:26:50 -0500

    Add appt. customer list populated

--- src/view_controller/AddAppointmentController.java
+++ src/view_controller/AddAppointmentController.java
@@ -1,5 +1,7 @@
 package view_controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,11 +11,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import model.Customer;
+import model.DBController;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -23,9 +31,23 @@ import static java.time.DayOfWeek.SUNDAY;
 
 public class AddAppointmentController implements Initializable {
 
+    @FXML private TableView<Customer> table_customers;
+    @FXML private TableColumn<Customer, Integer> table_customer_id;
+    @FXML private TableColumn<Customer, String> table_customer_name;
     @FXML private DatePicker datepicker_date;
 
     public void initialize(URL url, ResourceBundle rb) {
+
+        // populate customers table
+        ObservableList<Customer> allCustomersList = FXCollections.observableArrayList();
+        table_customer_id.setCellValueFactory(new PropertyValueFactory<Customer, Integer>("customerId"));
+        table_customer_name.setCellValueFactory(new PropertyValueFactory<Customer, String>("customerName"));
+        table_customers.refresh();
+        try {
+            table_customers.setItems(DBController.getCustomers());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         // disable dates in datepicker prior to today
         datepicker_date.setDayCellFactory(picker -> new DateCell() {
